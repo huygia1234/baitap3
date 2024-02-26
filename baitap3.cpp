59578c8c06e31a5bc703047a5e095c3874c40191
ADMIN <ADMIN@LAB1-MAY35>
2024-02-26 08:22:53 +0700

    Add project files.

---
+++ baitap3.cpp
@@ -0,0 +1,16 @@
+// baitap3.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+int main() {
+	int so1, so2, tong;
+	cout << "CHUONG TRINH TÍNH TONG\n";
+	cout << "nhap so thu nhat:";
+	cin >> so1;
+	cin >> so2;
+	tong = so1 + so2;
+	cout << so1 << "+" << so2 << "=" << tong << endl;
+	return 0;
+}
